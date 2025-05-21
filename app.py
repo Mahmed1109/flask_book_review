a22b6c7ddb7f0ae9cdbff3b0855b6391904b8543
Mahmed1109 <m.ahmed110900@gmail.com>
2025-05-21 10:46:33 +0100

    adding post resquest

--- app.py
+++ app.py
@@ -7,3 +7,15 @@ books = []
 @app.route('/')
 def index():
     return render_template('index.html', books=books)
+
+@app.route('/add', methods=['GET', 'POST'])
+def add_book():
+    if request.method == 'POST':
+        title = request.form['title']
+        author = request.form['author']
+        review = request.form['review']
+
+        books.append({'title': title, 'author': author, 'review': review})
+        return redirect(url_for('index'))
+
+    return render_template('add_book.html')
